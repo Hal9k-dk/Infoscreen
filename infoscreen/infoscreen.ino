63b51bf3825bea727fa88e6fd7fa70ce1d8b6df2
AndersV209 <andersv209@hotmail.com>
2015-07-31 15:44:08 +0200

    arduino code

--- infoscreen/infoscreen.ino
+++ infoscreen/infoscreen.ino
@@ -14,7 +14,7 @@ unsigned long lastmovemnt = 0;
 unsigned int offtime = 3600; //Seconds
 
 void setup() {
-Serial.begin(9600);
+//Serial.begin(9600);
 pinMode(relaypin, OUTPUT); 
 pinMode(ledpin, OUTPUT); 
 digitalWrite(relaypin, LOW);
@@ -28,23 +28,23 @@ pinMode(PIR_1, INPUT);
 }
 
 void loop() {
-Serial.print("Loop: ");
-Serial.println(lastmovemnt);
+//Serial.print("lastmovement: ");
+//Serial.println(lastmovemnt);
 
-Serial.print("PIR value: ");
-Serial.println(PIR_1);
+//Serial.print("PIR value: ");
+//Serial.println(PIR_1);
 
 if (digitalRead(PIR_1)){
   lastmovemnt = now();
   }
 
 if(now()- lastmovemnt < offtime){
-  Serial.println("IF");
+  //Serial.println("IF");
   digitalWrite(relaypin, HIGH);
   digitalWrite(ledpin, HIGH);
 }
 else if(now()- lastmovemnt > offtime){
-  Serial.println("ELSE");
+  //Serial.println("ELSE");
   digitalWrite(relaypin, LOW);
   digitalWrite(ledpin, LOW);
   }
@@ -56,7 +56,7 @@ delay(100);
 /*void movement(){
  detachInterrupt(0);
  //detachInterrupt(1);
- Serial.println("INTERRUPT");
+ //Serial.println("INTERRUPT");
  lastmovemnt = now();
 
  attachInterrupt(0, movement, RISING);
